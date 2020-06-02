4bd71dace008a950af7fcf30e4dec88da7d9db49
jinfenglin <ljfnwpusife@gmail.com>
2020-06-02 08:55:16 -0400

    debugging the header errors

--- model2/test.py
+++ model2/test.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 
 x = torch.tensor([[[.0, .0, .0], [1., 1., 1.], [2., 2., 2.], [3., 3., 3.]]])
-#torch.mean(x, dim=1)
 print(x.size())
+m = nn.AdaptiveAvgPool2d((1,4))
+print(m(x))
 pass
