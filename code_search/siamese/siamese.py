fix the acc overflow problem in online neg_sample and create sh files for crc use

code_search/siamese/siamese.py
@@ -87,8 +87,13 @@ def train_with_triplet_neg_sampling(args, model, train_examples: Examples, valid
                                     scheduler, tb_writer, step_bar, skip_n_steps):
     tr_loss, tr_ac = 0, 0
     batch_size = args.per_gpu_train_batch_size
+    cache_file = "cached_siamese_random_neg_sample_epoch_{}.dat".format(args.epochs_trained)
     if args.neg_sampling == "random":
-        train_dataloader = train_examples.random_triplet_dataloader(batch_size=batch_size)
+        if args.overwrite or not os.path.isfile(cache_file):
+            train_dataloader = train_examples.random_triplet_dataloader(batch_size=batch_size)
+            torch.save(train_dataloader, cache_file)
+        else:
+            train_dataloader = torch.load(cache_file)
     elif args.neg_sampling == "online":
         train_dataloader = train_examples.online_neg_sampling_dataloader(batch_size=batch_size)
     else:

code_search/single/single_train.py
@@ -28,7 +28,8 @@ def train_single_iteration(args, model, train_examples: Examples, valid_examples
             train_dataloader = torch.load(cache_file)
 
     elif args.neg_sampling == "online":
-        train_dataloader = train_examples.online_neg_sampling_dataloader(batch_size=batch_size)
+        # we provide only positive cases and will create negative in the batch processing
+        train_dataloader = train_examples.online_neg_sampling_dataloader(batch_size=batch_size / 2)
     else:
         raise Exception("{} neg_sampling is not recoginized...".format(args.neg_sampling))
 

code_search/twin/twin_train.py
@@ -61,13 +61,18 @@ def train_with_neg_sampling(args, model, train_examples: Examples, valid_example
 
     tr_loss, tr_ac = 0, 0
     batch_size = args.per_gpu_train_batch_size
+    cache_file = "cached_twin_random_neg_sample_epoch_{}.dat".format(args.epochs_trained)
     if args.neg_sampling == "random":
-        train_dataloader = train_examples.random_neg_sampling_dataloader(batch_size=batch_size)
+        if args.overwrite or not os.path.isfile(cache_file):
+            train_dataloader = train_examples.random_neg_sampling_dataloader(batch_size=batch_size)
+            torch.save(train_dataloader, cache_file)
+        else:
+            train_dataloader = torch.load(cache_file)
     elif args.neg_sampling == "offline":
         train_dataloader = train_examples.offline_neg_sampling_dataloader(model=model,
                                                                           batch_size=args.per_gpu_train_batch_size)
     elif args.neg_sampling == "online":
-        train_dataloader = train_examples.online_neg_sampling_dataloader(batch_size=batch_size)
+        train_dataloader = train_examples.online_neg_sampling_dataloader(batch_size=batch_size / 2)
     else:
         raise Exception("{} neg_sampling is not recoginized...".format(args.neg_sampling))
 

common/data_structures.py
@@ -348,7 +348,7 @@ class Examples:
                     pos.append((nl_id, pl_id, 1))
         neg_loader = DataLoader(cand_neg, batch_size=len(batch))
         neg_slots = len(pos)
-        hard_neg_slots = max(1, int(0.1 * neg_slots))
+        hard_neg_slots = max(1, int(0.7 * neg_slots))
         rand_neg_slots = max(0, neg_slots - hard_neg_slots)
         for neg_batch in neg_loader:
             with torch.no_grad():
@@ -375,11 +375,6 @@ class Examples:
             r_label.append(r[2])
         return (torch.Tensor(r_nl), torch.Tensor(r_pl), torch.Tensor(r_label).long())
 
-    def combine_pos_neg_to_triplet(self, pos_cases, neg_cases):
-        res = []
-        for p, n in zip(pos_cases, neg_cases):
-            pass
-        return res
 
     def make_online_triplet_sampling_batch(self, batch: Tuple, model: TwinBert):
         nl_ids = batch[0].tolist()