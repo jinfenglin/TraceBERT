afd5e765bc5554c1a30333a9af9d5753352debd7
jinfenglin <ljfnwpusife@gmail.com>
2020-12-31 21:40:28 -0500

    word embedding for code search net

--- code_search/trace_rnn/word2vec.py
+++ code_search/trace_rnn/word2vec.py
@@ -5,31 +5,30 @@ import pandas as pd
 from gensim.models import Word2Vec
 import re
 
+from data_processing import CodeSearchNetReader
+
 split_pattern = "\s|(?<!\d)[,.](?!\d)|//|\\n|\\\\|/|[\'=_\|]"
 
 
 def split_art(art_list):
     res = []
     for a in art_list:
-        if a!= a: # check nan
+        if a != a:  # check nan
             continue
         words = [word for word in re.split(split_pattern, a) if word and word != ' ']
-        print(words)
         res.append(words)
     return res
 
 
 def read_data():
     res = []
-    data_root = "../data/git_data"
-    for proj in ['dbcli/pgcli', 'keras-team/keras', 'pallets/flask']:
-        for part in ['train', 'valid', 'test']:
-            cm = pd.read_csv(os.path.join(data_root, proj, part, "commit_file"))
-            iss = pd.read_csv(os.path.join(data_root, proj, part, "issue_file"))
-            res.extend(split_art(cm['diff']))
-            res.extend(split_art(cm['summary']))
-            res.extend(split_art(iss['issue_comments']))
-            res.extend(split_art(iss['issue_desc']))
+    data_root = "../data/code_search_net/python"
+    csn_reader = CodeSearchNetReader(data_root)
+    for type in ["train", "test", "valid"]:
+        raw_examples = csn_reader.get_examples(type=type, summary_only=True)
+        for e in raw_examples:
+            res.extend(split_art([e['NL']]))
+            res.extend(split_art([e['PL']]))
     return res
 
 
@@ -37,7 +36,6 @@ if __name__ == "__main__":
     lines = read_data()
     print("load data with {} lines".format(len(lines)))
     model = Word2Vec(sentences=lines, size=100, window=5, min_count=1, workers=4, iter=20)
-    model.wv.save("proj_vocab.wordvectors")
     with open("./we/proj_embedding.txt", 'w', encoding='utf8') as fout:
         for w in model.wv.vocab:
             embd = " ".join([str(x) for x in model.wv[w]])
